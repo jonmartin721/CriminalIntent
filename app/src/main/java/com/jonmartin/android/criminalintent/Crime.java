4cc07e058b2d69d0f51a8a83d4315b90c3c848ab
Jonathan Martin <jonmartin721@gmail.com>
2018-02-12 16:49:35 -0600

    Work so far in CriminalIntent

---
+++ app/src/main/java/com/jonmartin/android/criminalintent/Crime.java
@@ -0,0 +1,52 @@
+package com.jonmartin.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by martinjo on 2/12/2018.
+ */
+
+public class Crime {
+
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+
+    //constructor
+    public Crime() {
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
