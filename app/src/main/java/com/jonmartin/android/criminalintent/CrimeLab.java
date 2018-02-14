f8bcafec6800bd1d35bd4242c67f6394fbdd0b3e
Jonathan <jonmartin721@gmail.com>
2018-02-13 22:20:57 -0600

    Work on Chapter 8 so far, almost done.

---
+++ app/src/main/java/com/jonmartin/android/criminalintent/CrimeLab.java
@@ -0,0 +1,49 @@
+package com.jonmartin.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+
+    //variables
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime # " + i);
+            crime.setSolved(i % 2 == 0); //every other one will be marked solved
+            mCrimes.add(crime);
+        }
+    }
+
+    //returns the list of crimes
+    public List<Crime> getmCrimes() {
+        return mCrimes;
+    }
+
+    //returns specific crimes from the UUID passed to it
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getID().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
+
+}
